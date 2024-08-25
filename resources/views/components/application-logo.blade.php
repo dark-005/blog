2184c66b3081708090fd566bda9a6c991c560e49
dark-005 <79380388+dark-005@users.noreply.github.com>
2024-08-25 18:55:13 -0500

    blog v1

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,3 @@
+<svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    <image xlink:href="{{ asset('images/logo.png')}}" width="316" height="316"/>
+</svg>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+// Se agruparon las rutas de un mismo controlador
+Route::controller(PageController::class)->group(function(){
+
+    Route::get('/',          'home')->name('home');
+
+    
+    Route::get('blog/{post:slug}','post')->name('post');
+});
+
+Route::redirect('dashboard', 'posts')->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('posts', PostController::class)->middleware(['auth', 'verified'])->except('show');
+require __DIR__.'/auth.php';
